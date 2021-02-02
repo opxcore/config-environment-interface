f8c28ad72fb5adb9c35f2f0545551c6e6e6ce10e
lozovoyv <lozovoyv@gmail.com>
2021-02-02 12:03:02 +0300

    Add methods

--- src/Interfaces/EnvironmentInterface.php
+++ src/Interfaces/EnvironmentInterface.php
@@ -13,7 +13,7 @@ namespace OpxCore\Config\Interfaces;
 interface EnvironmentInterface
 {
     /**
-     * Gets value from environment.
+     * Get value from environment.
      *
      * @param string $key
      * @param null|callable|mixed $default
@@ -21,4 +21,33 @@ interface EnvironmentInterface
      * @return  array|bool|string|null|mixed
      */
     public function get(string $key, $default = null);
+
+    /**
+     * Set value to environment.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param bool $safe
+     *
+     * @return  bool
+     */
+    public function set(string $key, $value, bool $safe = false): bool;
+
+    /**
+     * Whether a offset exists.
+     *
+     * @param string $key
+     *
+     * @return  bool
+     */
+    public function has(string $key): bool;
+
+    /**
+     * Offset to unset.
+     *
+     * @param string $key
+     *
+     * @return  void
+     */
+    public function unset(string $key): void;
 }
